cc8e9e159ba104e5def89a7ff28c64c8bc15ed15
Simon Ko <simonko86@gmail.com>
2018-04-27 15:15:54 -0400

    adding modified code

--- CppApplication_1/main.cpp
+++ CppApplication_1/main.cpp
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: Simon
- *
- * Created on April 27, 2018, 3:07 PM
+ * I am using this as OO practice 
+ * Created on April 14, 2018, 10:17 AM
  */
 
 #include <cstdlib>
@@ -16,11 +10,64 @@
 
 using namespace std;
 
+class Geeks
+{
+public:
+  
+    void printname()
+    {
+        newgeekname = changename(geekname,geeksurname);
+        cout << "Geekname is: " << newgeekname;
+    }
+    
+    void setname(string name)
+    {
+        this->geekname = name;
+    }
+    
+    void setsurname(string name)
+    {
+        this->geeksurname = name;
+    }
+    
+private:
+    string geekname;
+    string geeksurname;
+    string newgeekname;
+
+    string getname()
+    {
+        return geekname;
+    }
+   
+    string changename(string geekname, string geeksurname)
+    {
+        newgeekname = geekname + " " + geeksurname;
+        return newgeekname;
+    }
+};
 /*
  * 
  */
 int main(int argc, char** argv) {
-    cout << "Hello world";
+    Geeks first;
+    Geeks second;
+    string name;
+    string surname;
+    
+    
+    cout << "Please enter a name: \n";
+    cin >> name;
+    cout << "Please enter a surname: \n";
+    cin >> surname;
+    first.setname(name);
+    first.setsurname(surname);
+    
+    second = first; //set the 2 objects together
+    second.printname();
+
+
+    
+    
     return 0;
 }
-
